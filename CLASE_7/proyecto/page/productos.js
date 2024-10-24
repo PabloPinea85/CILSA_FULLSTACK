c546569bff19da8c96d3837dae46990e293676aa
Axelzalazar <axelzalazar15@gmail.com>
2024-10-24 14:30:33 -0300

    Rediseño de home

--- CLASE_7/proyecto/page/productos.js
+++ CLASE_7/proyecto/page/productos.js
@@ -29,8 +29,10 @@ function mostrarBikes(bikes) {
     const cardHTML = `
       <div class="col-sm-6 col-md-4 col-lg-3 mb-4">
         <div class="card h-100 shadow-sm border-0 rounded-3 overflow-hidden transform-scale hover-shadow">
-          <img src="${bike.imagen}" class="card-img-top" alt="${bike.biciicleta}" />
-          <div class="card-body d-flex flex-column">
+          <div class="card-img-top">
+            <img src="${bike.imagen}" alt="${bike.biciicleta}" />
+          </div>
+          <div class="card-body h-50 d-flex flex-column ">
             <h5 class="mt-auto card-title text-center">${bike.marca}</h5>
             <p class="card-text text-center">${bike.biciicleta}</p>
             <div class="text-center">
